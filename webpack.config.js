e6dda80a9d48fdbf8f7863509d3cb161789eb23c
Michał Łyczko <lyczos@gmail.com>
2019-02-25 13:58:27 -0800

    add webpack css builder

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,6 @@ module.exports = (env = {}) => {
       ]
     },
     externals: { grapesjs: 'grapesjs' },
-    plugins: [...plugins, new ExtractTextPlugin('dist/style.css')]
+    plugins: [...plugins, new ExtractTextPlugin('dist/grapesjs-knowde-plugin.css')]
   };
 };
